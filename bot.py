0b8fab0e210a7a8b527beb3d98587a1a6c5d0ca9
lyt99 <lyt1999131@163.com>
2024-11-06 14:10:06 +0800

    add asgi

--- bot.py
+++ bot.py
@@ -1,8 +1,6 @@
 import nonebot
 from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
 
-
-
 nonebot.init()
 
 driver = nonebot.get_driver()
@@ -10,6 +8,8 @@ driver.register_adapter(ONEBOT_V11Adapter)
 
 
 nonebot.load_from_toml("pyproject.toml")
+
+app = nonebot.get_asgi()
 
 if __name__ == "__main__":
     nonebot.run()
